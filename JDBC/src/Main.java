8e859dbd95a2916f4f6d04b7821ce965b58a0968
tanmayyyy <tanmaykhanna07@gmail.com>
2026-03-17 22:01:18 +0530

    Printed the accounts table data

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -2,7 +2,7 @@ import java.sql.*;
 
 public class Main {
 
-    private static final String url = "jdbc:mysql://localhost:3306/?user=root";
+    private static final String url = "jdbc:mysql://localhost:3306/simple_bank";
     private static final String username = "root";
     private static final String password = "khanna2007";
 
@@ -10,16 +10,32 @@ public class Main {
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch(ClassNotFoundException e){
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
 
         try{
             Connection connection = DriverManager.getConnection(url,username,password);
             Statement statement = connection.createStatement();
+            String query = "SELECT * FROM accounts";
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next()){
+                int account_id = resultSet.getInt("account_id");
+                int customer_id = resultSet.getInt("customer_id");
+                double balance = resultSet.getDouble("balance");
+                String account_type = resultSet.getString("account_type");
+                System.out.println("Account ID: " + account_id);
+                System.out.println("Customer ID: " + customer_id);
+                System.out.println("Account type: " + account_type);
+                System.out.println("Balance: " + balance);
+                System.out.println();
+            }
+
+
+
         }catch (SQLException e){
             System.out.println(e.getMessage());
         }
     }
 
 }
-jkj;aljoip
