b62be30fb2c34143b1813c980d70b90131ed10d4
tanmayyyy <tanmaykhanna07@gmail.com>
2026-03-30 21:48:47 +0530

    Made a program to read the updated workstations table using preparedStatements

---
+++ Java_JDBC/src/main/java/LabFileReading.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class LabFileReading {
+    private static final String url = "jdbc:mysql://localhost:3306/college";
+    private static final String username = "root";
+    private static final String password = "khanna2007";
+    public static void main(String[] args){
+        try{
+            Connection connection = DriverManager.getConnection(url,username,password);
+            String query = "SELECT * FROM workstations WHERE lab_id = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1,7);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                System.out.println("Station: " + resultSet.getString("station_name"));
+                System.out.println("Status: " + resultSet.getString("status"));
+                System.out.println(" ");
+            }
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
